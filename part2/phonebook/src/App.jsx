2e218cb857e5729d615b44a745e5ad78444191d3
MorisV <maurice.mecke@yahoo.de>
2025-04-16 23:18:14 +0200

    finished 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Person from "./components/Person";
 import PersonsForm from "./components/PersonsForm";
 import SearchFilter from "./components/SearchFilter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [filteredPersons, setNewFilteredPersons] = useState(persons);
-  //const [filterdPerson, setFilteredPerson] =
+  const [filteredPersons, setNewFilteredPersons] = useState([]);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("fullfilled");
+      setPersons(response.data);
+      setNewFilteredPersons(response.data);
+    });
+  };
+  //eventHandler to call the hook function
+  useEffect(hook, []);
+
   const addNewPerson = (event) => {
     event.preventDefault();
 
