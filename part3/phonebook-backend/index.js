864547a8353e0ff4fb3b633e9ad2130f00134d55
MorisV <maurice.mecke@yahoo.de>
2025-05-07 22:31:42 +0200

    prepping for 3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config;
 const app = express();
-app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(
   morgan(function (tokens, req, res) {
